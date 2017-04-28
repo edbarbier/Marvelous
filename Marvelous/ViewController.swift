f50347be2d6e058a698bf9b07bcea5858d716757
Edouard Barbier <edouardbarbier0@gmail.com>
2017-04-28 16:53:03 +0100

    Initial Commit

---
+++ Marvelous/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marvelous
+//
+//  Created by Edouard Barbier on 28/04/17.
+//  Copyright © 2017 Edouard Barbier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
