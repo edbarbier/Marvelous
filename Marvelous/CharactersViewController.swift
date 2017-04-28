bf1bc0ff8610c2e372f127153078b7fa78005f84
Edouard Barbier <edouardbarbier0@gmail.com>
2017-04-28 20:57:04 +0100

    Added UI for collectionView screen

---
+++ Marvelous/CharactersViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CharactersViewController.swift
+//  Marvelous
+//
+//  Created by Edouard Barbier on 28/04/17.
+//  Copyright © 2017 Edouard Barbier. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+    
+
+    
+}
